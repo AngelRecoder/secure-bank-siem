8911db125d7a0e30a782774652173365141735bc
Angel <Angel_Rec01@outlook.com>
2026-03-09 20:51:39 -0600

    feat: add Spring Boot backend with security dependencies

---
+++ backend/secure-bank/src/main/java/com/angel/secure_bank/SecureBankApplication.java
@@ -0,0 +1,13 @@
+package com.angel.secure_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureBankApplication.class, args);
+	}
+
+}
